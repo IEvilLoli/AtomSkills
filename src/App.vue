db294c2a6c0a8a6e9dfc9613fc12885ce38d8d83
gorbushin_v_a <gorbushin.volodja@mail.ru>
2023-06-09 13:08:48 +0300

    delete setup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Autorization from './components/Autorization.vue'
+import TablePage from './components/TablePage.vue'
 </script>
 
 <template>
  <Autorization></Autorization>
+ <TablePage></TablePage>
 </template>
 
 <style scoped>
